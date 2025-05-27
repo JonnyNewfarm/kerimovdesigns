1d7b05ef40eeeaf8e877a40327f1525700ae69e5
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-05-27 04:25:35 +0100

    .

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -3,8 +3,7 @@ import React, { useRef } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { Mesh, TextureLoader } from "three";
 
-import { OrbitControls } from "@react-three/drei";
-import { useScroll, useSpring, useTransform } from "framer-motion";
+import { useScroll, useSpring, useTransform, motion } from "framer-motion";
 import Link from "next/link";
 
 export default function Index() {
@@ -25,24 +24,60 @@ export default function Index() {
           <Cube scrollProgress={smoothProgress} />
         </Canvas>
 
-        <div className="absolute bottom-15 text-center  px-4">
+        <motion.div
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{
+            scale: [0.6, 1],
+            opacity: [0, 1, 1],
+          }}
+          transition={{
+            duration: 2,
+            times: [0, 0.4, 1],
+            ease: "easeInOut",
+          }}
+          className="absolute bottom-15 text-center  px-4"
+        >
           <h1 className="text-white/90 text-2xl sm:text-2xl  font-bold">
             Rustam Kerimov
           </h1>
           <h2 className="text-white/90 text-3xl whitespace-nowrap sm:text-4xl font-extrabold">
             Graphic Designer
           </h2>
-        </div>
-        <div className="absolute hidden md:block left-10 bottom-15 text-left  px-4">
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{
+            scale: [0.6, 1],
+            opacity: [0, 1, 1],
+          }}
+          transition={{
+            duration: 2.4,
+            times: [0, 0.4, 1],
+            ease: "easeInOut",
+          }}
+          className="absolute hidden md:block left-10 bottom-15 text-left  px-4"
+        >
           <h2 className="text-white/90 text-4xl sm:text-4xl font-extrabold">
             <Link href={"/projects"}>Archives</Link>
           </h2>
-        </div>
-        <div className="absolute hidden md:block right-10 bottom-15 text-left  px-4">
+        </motion.div>
+        <motion.div
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{
+            scale: [0.6, 1],
+            opacity: [0, 1, 1],
+          }}
+          transition={{
+            duration: 2.4,
+            times: [0, 0.4, 1],
+            ease: "easeInOut",
+          }}
+          className="absolute hidden md:block right-10 bottom-15 text-left  px-4"
+        >
           <h2 className="text-white/90 text-4xl sm:text-4xl font-extrabold">
             <Link href={"/contact"}> Collaborate</Link>
           </h2>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
@@ -51,12 +86,12 @@ export default function Index() {
 const Cube = ({ scrollProgress }: { scrollProgress: any }) => {
   const mesh = useRef<Mesh>(null);
 
-  const texture1 = useLoader(TextureLoader, "/cube/image1.jpg");
-  const texture2 = useLoader(TextureLoader, "/cube/image2.jpg");
-  const texture3 = useLoader(TextureLoader, "/cube/image3.jpg");
-  const texture4 = useLoader(TextureLoader, "/cube/image4.jpg");
-  const texture5 = useLoader(TextureLoader, "/cube/image.jpg");
-  const texture6 = useLoader(TextureLoader, "/cube/image6.jpg");
+  const texture1 = useLoader(TextureLoader, "/cube-img/image1.jpg");
+  const texture2 = useLoader(TextureLoader, "/cube-img/image2.jpg");
+  const texture3 = useLoader(TextureLoader, "/cube-img/image3.jpg");
+  const texture4 = useLoader(TextureLoader, "/cube-img/image4.jpg");
+  const texture5 = useLoader(TextureLoader, "/cube-img/image5.jpg");
+  const texture6 = useLoader(TextureLoader, "/cube-img/image6.jpg");
 
   useFrame(() => {
     if (!mesh.current) return;
