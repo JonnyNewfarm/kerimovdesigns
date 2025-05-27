0038eaaeb10e90dadde636758aaf343415547738
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-05-27 06:03:37 +0100

    .

--- src/components/MyProjects.tsx
+++ src/components/MyProjects.tsx
@@ -12,6 +12,9 @@ interface MyProjectsProps {
 const MyProjects = ({ projects }: MyProjectsProps) => {
   const [hoveredProject, setHoveredProject] = useState<number>(0);
 
+  const firstRow = projects.slice(0, 3);
+  const secondRow = projects.slice(3, 5);
+
   return (
     <div className="bg-[#242323] relative flex flex-col justify-center items-center h-screen w-full text-[#ecebeb]">
       <div className="md:w-[500px] md:h-[300px] w-[350px] h-[200px] relative">
@@ -22,12 +25,13 @@ const MyProjects = ({ projects }: MyProjectsProps) => {
           alt={projects[hoveredProject].title}
         />
       </div>
-      <div className="mt-10">
-        <h1 className="uppercase font-semibold text-2xl">Previous work</h1>
+      <div className="mt-8">
+        <h1 className="uppercase  text-2xl">Previous work</h1>
       </div>
 
-      <div className="flex flex-wrap  gap-x-4 gap-y-2 mt-10 justify-center items-center text-lg font-bold uppercase">
-        {projects.slice(0, 5).map((project, index) => (
+      {/* First row */}
+      <div className="flex gap-x-4 mt-8 justify-center items-center text-lg font-bold uppercase">
+        {firstRow.map((project, index) => (
           <React.Fragment key={project.title}>
             <Link
               href={`/project/${project.id}`}
@@ -40,7 +44,29 @@ const MyProjects = ({ projects }: MyProjectsProps) => {
             >
               {project.title}
             </Link>
-            {index !== projects.length - 1 && (
+            {index !== firstRow.length - 1 && (
+              <span className="text-[#ecebeb]">-</span>
+            )}
+          </React.Fragment>
+        ))}
+      </div>
+
+      {/* Second row */}
+      <div className="flex gap-x-4  justify-center items-center text-lg font-bold uppercase">
+        {secondRow.map((project, index) => (
+          <React.Fragment key={project.title}>
+            <Link
+              href={`/project/${project.id}`}
+              className={`px-2 py-2 transition ${
+                hoveredProject === index + 3
+                  ? "text-[#7f7979] scale-110"
+                  : "hover:text-[#ecebeb]"
+              }`}
+              onMouseEnter={() => setHoveredProject(index + 3)}
+            >
+              {project.title}
+            </Link>
+            {index !== secondRow.length - 1 && (
               <span className="text-[#ecebeb]">-</span>
             )}
           </React.Fragment>
