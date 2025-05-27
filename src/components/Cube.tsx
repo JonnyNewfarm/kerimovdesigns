a6a2271c13754dc861834a2527298df866e5fa0a
Jonas Nygaard <jonasnygaard96@gmail.com>
2025-05-27 02:49:24 +0100

    .

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,52 +1,60 @@
 "use client";
-import React, { useEffect, useRef } from "react";
-import { Canvas, useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
-import { OrbitControls } from "@react-three/drei";
-import { useSpring, useScroll, useTransform } from "framer-motion";
+import React, { useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Mesh, TextureLoader } from "three";
+import { OrbitControls, ScrollControls } from "@react-three/drei";
+import { useScroll } from "framer-motion";
 
-export default function index() {
+export default function Index() {
   const container = useRef(null);
+
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ["start start", "end end"],
   });
-  const progress = useTransform(scrollYProgress, [0, 1], [0, 5]);
-  const smoothProgress = useSpring(progress, { damping: 20 });
 
   return (
-    <div ref={container} className={"h-[200vh]"}>
-      <div className={"sticky top-0 h-screen"}>
+    <div ref={container} className="h-[150vh]">
+      <div className="h-screen sticky top-0">
         <Canvas>
           <OrbitControls enableZoom={false} enablePan={false} />
           <ambientLight intensity={2} />
           <directionalLight position={[2, 1, 1]} />
-          <Cube progress={smoothProgress} />
+          <Cube scrollProgress={scrollYProgress} />
         </Canvas>
       </div>
     </div>
   );
 }
 
-function Cube({ progress }: any) {
-  const mesh = useRef(null);
+const Cube = ({ scrollProgress }: { scrollProgress: any }) => {
+  const mesh = useRef<Mesh>(null);
 
-  const texture_1 = useLoader(TextureLoader, "/cube/img1.jpg");
-  const texture_2 = useLoader(TextureLoader, "/cube/img2.jpg");
-  const texture_3 = useLoader(TextureLoader, "/cube/img3.jpg");
-  const texture_4 = useLoader(TextureLoader, "/cube/img4.jpg");
-  const texture_5 = useLoader(TextureLoader, "/cube/img5.jpg");
-  const texture_6 = useLoader(TextureLoader, "/cube/img6.jpg");
+  useFrame(() => {
+    if (mesh.current) {
+      const offset = scrollProgress.get();
+      mesh.current.rotation.x = offset * 5;
+      mesh.current.rotation.y = offset * 5;
+      mesh.current.rotation.z = offset * 5;
+    }
+  });
+
+  const texture1 = useLoader(TextureLoader, "/cube/img1.jpg");
+  const texture2 = useLoader(TextureLoader, "/cube/img2.jpg");
+  const texture3 = useLoader(TextureLoader, "/cube/img3.jpg");
+  const texture4 = useLoader(TextureLoader, "/cube/img4.jpg");
+  const texture5 = useLoader(TextureLoader, "/cube/img5.jpg");
+  const texture6 = useLoader(TextureLoader, "/cube/img6.jpg");
 
   return (
-    <mesh ref={mesh} rotation-y={progress} rotation-x={progress}>
-      <boxGeometry args={[2.5, 2.5, 2.5]} />
-      <meshStandardMaterial map={texture_1} attach="material-0" />
-      <meshStandardMaterial map={texture_2} attach="material-1" />
-      <meshStandardMaterial map={texture_3} attach="material-2" />
-      <meshStandardMaterial map={texture_4} attach="material-3" />
-      <meshStandardMaterial map={texture_5} attach="material-4" />
-      <meshStandardMaterial map={texture_6} attach="material-5" />
+    <mesh ref={mesh}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial map={texture1} attach={"material-0"} />
+      <meshStandardMaterial map={texture2} attach={"material-1"} />
+      <meshStandardMaterial map={texture3} attach={"material-2"} />
+      <meshStandardMaterial map={texture4} attach={"material-3"} />
+      <meshStandardMaterial map={texture5} attach={"material-4"} />
+      <meshStandardMaterial map={texture6} attach={"material-5"} />
     </mesh>
   );
-}
+};
