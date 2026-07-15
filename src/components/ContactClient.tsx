d016772fedb563c18fd23dd64888cc7b8e7d032a
Jonas Nygaard <jonasnygaard96@gmail.com>
2026-07-15 13:32:45 +0100

    :

--- src/components/ContactClient.tsx
+++ src/components/ContactClient.tsx
@@ -84,7 +84,7 @@ function SentenceInput({
       onChange={onChange}
       placeholder={placeholder}
       required={required}
-      className={`mx-2 mb-2 inline-block min-w-[220px] max-w-full border-0 bg-transparent px-1 pb-1 text-[0.82em] font-black leading-none text-white outline-none transition placeholder:text-[#bcc4ae] focus:text-color md:min-w-[320px] ${className}`}
+      className={`mx-2 mb-2 inline-block min-w-[220px] max-w-full border-0 bg-transparent px-1 pb-1 text-[0.82em] font-black leading-none text-white outline-none transition placeholder:text-[#79895c]/70 focus:text-color md:min-w-[320px] ${className}`}
     />
   );
 }
@@ -120,7 +120,7 @@ function SentenceTextarea({
         scrollbarColor: "rgba(245, 236, 220, 0.35) transparent",
         scrollbarWidth: "thin",
       }}
-      className="max-h-[360px] min-h-[100px] w-full resize-none overflow-y-auto border-0 bg-transparent px-0 pb-4 pr-5 text-[clamp(2rem,4.4vw,5rem)] font-black normal-case leading-[1.08] tracking-[-0.035em] text-[#a3b18a] outline-none transition placeholder:text-[#a3b18a]/65 focus:text-color [&::-webkit-scrollbar]:w-[6px] [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-color/25 [&::-webkit-scrollbar-thumb]:transition [&::-webkit-scrollbar-thumb:hover]:bg-color/45"
+      className="max-h-[360px] min-h-[100px] w-full resize-none overflow-y-auto border-0 bg-transparent px-0 pb-4 pr-5 text-[clamp(2rem,4.4vw,5rem)] font-black normal-case leading-[1.08] tracking-[-0.035em] text-[#a3b18a] outline-none transition placeholder:text-[#79895c]/70 focus:text-color [&::-webkit-scrollbar]:w-[6px] [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-color/25 [&::-webkit-scrollbar-thumb]:transition [&::-webkit-scrollbar-thumb:hover]:bg-color/45"
     />
   );
 }
